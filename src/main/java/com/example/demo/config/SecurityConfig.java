e2fe04115532773ccec0903df87c24193092f719
Triet <truongbinhtriet110202@gmail.com>
2023-11-26 10:01:49 +0700

    lab week 8 security

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+
+package com.example.demo.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Autowired
+    public void globalConfig(AuthenticationManagerBuilder auth, PasswordEncoder encoder, DataSource dataSource)throws Exception{
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(User.withUsername("admin")
+                        .password(encoder.encode("admin"))
+                        .roles("ADMIN")
+                        .build())
+                .withUser(User.withUsername("teo")
+                        .password(encoder.encode("teo"))
+                        .roles("TEO")
+                        .build())
+                .withUser(User.withUsername("ty")
+                        .password(encoder.encode("ty"))
+                        .roles("USER")
+                        .build())
+        ;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth->auth
+                        .requestMatchers("/","/home","/index").permitAll()
+                        .requestMatchers("/api/**").hasAnyRole("ADMIN","USER","TEO")
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(("/admin/**")).hasRole("ADMIN")
+                        .anyRequest().authenticated()
+        );
+        http.csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"));
+        http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
